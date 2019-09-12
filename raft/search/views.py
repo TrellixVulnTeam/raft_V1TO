0c6f54d96600554e7776e298995ccfa6db2637ec
Jonathan Chen <jchen7@uw.edu>
2019-09-11 23:01:50 -0700

    added link redirect to /search

--- raft/search/views.py
+++ raft/search/views.py
@@ -8,7 +8,6 @@ from .models import ViewedRafts
 
 from .forms import SearchForm
 
-# Create your views here.
 def home(request):
     #potential_RAFTs = check_restaurants(location, cuisine)
     #num_RAFTs = len(potential_RAFTs)
@@ -30,41 +29,41 @@ def home(request):
                                #address=address, city=city, state=state, zip_code=zip_code)
 
     #rafts = ViewedRafts.objects.all()
+    
+    form = SearchForm(request.GET)
+    return render(request, 'home.html', {'form': form})
 
-    if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            return search(request, form)
+def search(request):
+    form = SearchForm(request.POST)
+
+    if form.is_valid():
+        term = form.cleaned_data['term']
+        location = form.cleaned_data['location']
+
+        potential_RAFTs = check_restaurants(location, term)
+
+        num_RAFTs = len(potential_RAFTs)
+        random_index = random.randint(0, num_RAFTs - 1)
+
+        potential_RAFT = potential_RAFTs[random_index]
+        name = potential_RAFT.name
+        address = potential_RAFT.address
+        price = potential_RAFT.price
+        rating = potential_RAFT.rating
+        image_url = potential_RAFT.image_url
+
+        args = {
+            'form': form,
+            'name': name,
+            'address': address,
+            'price': price,
+            'rating': rating,
+            'image_url': image_url,
+            }
+
+        return render(request, 'search.html', args)
     else:
-        form = SearchForm(request.GET)
-        return render(request, 'home.html', {'form': form})
-
-def search(request, form):
-    term = form.cleaned_data['term']
-    location = form.cleaned_data['location']
-
-    potential_RAFTs = check_restaurants(location, term)
-
-    num_RAFTs = len(potential_RAFTs)
-    random_index = random.randint(0, num_RAFTs - 1)
-
-    potential_RAFT = potential_RAFTs[random_index]
-    name = potential_RAFT.name
-    address = potential_RAFT.address
-    price = potential_RAFT.price
-    rating = potential_RAFT.rating
-    image_url = potential_RAFT.image_url
-
-    args = {
-        'form': form,
-        'name': name,
-        'address': address,
-        'price': price,
-        'rating': rating,
-        'image_url': image_url,
-        }
-
-    return render(request, 'search.html', args)
+        return render(request, 'search.html', {'form': form})
 
 def about(request):
     return render(request, 'about.html')
